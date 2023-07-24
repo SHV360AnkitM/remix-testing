e6d2c8fa4707ba66896ad7fc58693a930f6d58d3
Ankit Meshram <ankitdm69@gmail.com>
2023-07-24 18:27:45 +0530

    testing

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const _index = () => {
   return (
-    <div>Hello World {process.env.HELLO ? process.env.HELLO : window.env.HELLO}</div>
+    <div>Hello World {process.env.HELLO ? process.env.HELLO : window.env.HELLO ? window.env.HELLO : "No env's working"}</div>
   )
 }
 
